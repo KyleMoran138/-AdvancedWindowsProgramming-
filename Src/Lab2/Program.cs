29c65695ae8465bfee7e126e96c97ca0ed7ba346
kyle <kylemoranp@gmail.com>
2018-01-17 20:34:16 -0500

    Complete Lab2

---
+++ Src/Lab2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Please enter a sentence:");
+            var input = Console.ReadLine();
+            Console.WriteLine("\n You entered: \n " + input + "\n");
+            Console.Write("Distinct words: ");
+
+            foreach(String s in input.Split().Distinct()) {
+                Console.Write(s + " ");
+            }
+
+            Console.WriteLine("\n Press any key to continue...");
+            Console.ReadKey();
+
+
+        }
+    }
+}
