0773b6bc169db3b15c280901421e657880c3ff32
kyle <kylemoranp@gmail.com>
2018-01-31 15:05:38 -0500

    Started working on Lab100-1

---
+++ Src/Lab100-1/DataList.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab100_1 {
+    class DataList {
+        private List<Person> personList;
+        public string FileName; 
+        public DataList(string fname) {
+            FileName = fname;
+            personList = new List<Person>();
+        }
+        public Person FindPerson(String lastName) {
+            var person = from p in personList
+                         where p.LastName == lastName
+                         select p;
+            return person.FirstOrDefault();
+        }
+        public Person GetPersonAt(int i) {
+            return personList.ElementAt(i);
+        }
+        public Person FindPerson(Person otherPerson) {
+            var per = from p in personList
+                      where p.Equals(otherPerson)
+                      select p;
+            return personList.FirstOrDefault();
+        }
+        public string FindPersonsAddress(Person otherPerson) {
+            string addr = null;
+            var persons = from p in personList
+                          where p.Equals(otherPerson)
+                          select p;
+            Person p1 = persons.FirstOrDefault();
+            if(p1 != null) {
+                addr = string.Format($"{p1.FirstName}'s address is: " + $"{p1.Address} {p1.City} {p1.State} {p1.Zip}");
+            }
+            return addr;
+        }
+        public List<Person> FindPeropleWithSameLastName(string lastname) {
+            return (from p in personList
+                   where p.LastName.Equals(lastname)
+                   select p).ToList();
+        }
+        public void InsertPerson(String first, String last, String addr, String city, String state, int zip) {
+            personList.Add(new Lab100_1.Person(first, last, addr, city, state, zip));
+            return;
+        }
+        public void DisplayPeopleByCity(string c) {
+            var byCity = from p in personList
+                         where p.City == c
+                         select p;
+            byCity.ToList<Person>().ForEach(x => Console.WriteLine(x));
+            return;
+        }
+        public void DisplayPeopleByCityAndState(string c, string s) {
+            var byCity = from p in personList
+                         where (p.City == c && p.State == s)
+                         select p;
+            byCity.ToList<Person>().ForEach(x => Console.WriteLine(x));
+            return;
+        }
+        public bool DeletePerson(Person thisPerson) => personList.Remove(thisPerson);
+        public void DisplayPeople() {
+            int i = 0;
+            personList.ForEach(x => Console.WriteLine($"{++i,6}) {x}"));
+        }
+
+    }
+}
---
+++ Src/Lab100-1/Person.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab100_1 {
+    class Person {
+        public String FirstName;
+        public String LastName;
+        public String Address;
+        public String City;
+        public String State;
+        public int Zip;
+
+        public Person(String first, String last, String addr, String city, String state, int zip) {
+            this.FirstName = first;
+            this.LastName = last;
+            this.Address = addr;
+            this.City = city;
+            this.State = state;
+            this.Zip = zip;
+        }
+
+        public Person() : this(string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, 0) { }
+
+        public bool Equals(Person other) {
+            if (this == null) return false;
+            if (this == other) return true;
+            if(other == null || this.GetType() != other.GetType())
+                return false;
+            if (this.FirstName == null && other.FirstName == null) {
+                return false;
+            } else if (!FirstName.Equals(other.FirstName)) {
+                return false;
+            }
+
+            if (this.LastName == null && other.LastName == null) {
+                return false;
+            } else if (!LastName.Equals(other.LastName)) {
+                return false;
+            }
+
+            return true;
+        }
+
+        public int CompareTo(Person other) {
+            if (this.LastName.Equals(other.LastName)) {
+                return this.FirstName.CompareTo(other.FirstName);
+            }
+            return this.LastName.CompareTo(other.LastName);
+        }
+
+        public override string ToString() =>
+            string.Format("{0,12} {1,-12} {2,30} {3,-24} {4,-6} {5:d5}", this.FirstName, this.LastName, this.Address, this.City, this.State, this.Zip);
+
+    }
+}
